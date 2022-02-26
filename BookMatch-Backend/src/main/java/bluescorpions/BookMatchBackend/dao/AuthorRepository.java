043ba8f84d20a5829c95a55787c02249f273e26d
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-02-25 22:50:07 -0500

    Fixing author repo

--- BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/dao/AuthorRepository.java
+++ BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/dao/AuthorRepository.java
@@ -1,6 +1,7 @@
 package bluescorpions.BookMatchBackend.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import bluescorpions.BookMatchBackend.model.Author;
 
 public interface AuthorRepository extends CrudRepository<Author, String>{
 
