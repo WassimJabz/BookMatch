1aa939fc7cc5e2f674bf61cc74311a39a0f6b014
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-02-25 20:53:56 -0500

    Added wrapper

--- BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/BookMatchBackendApplication.java
+++ BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/BookMatchBackendApplication.java
@@ -1,13 +1,20 @@
 package bluescorpions.BookMatchBackend;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class BookMatchBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMatchBackendApplication.class, args);
 	}
+
+	@RequestMapping("/")
+  	public String greeting(){
+    return "Hello world!";
+  }
 
 }
