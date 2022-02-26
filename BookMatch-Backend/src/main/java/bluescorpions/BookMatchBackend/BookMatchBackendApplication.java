4811c411e5e7e273bde2c1054db18ece7b1785e4
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-02-25 20:40:32 -0500

    Initialize backend

---
+++ BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/BookMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package bluescorpions.BookMatchBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMatchBackendApplication.class, args);
+	}
+
+}
---
+++ BookMatch-Backend/src/test/java/bluescorpions/BookMatchBackend/BookMatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bluescorpions.BookMatchBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMatchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
