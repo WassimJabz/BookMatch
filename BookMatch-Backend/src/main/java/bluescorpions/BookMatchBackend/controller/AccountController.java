5e559f85129752225c1f9aab1216e616d0f2f5cf
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-02-27 02:44:46 -0500

    Update AccountController.java

--- BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/controller/AccountController.java
+++ BookMatch-Backend/src/main/java/bluescorpions/BookMatchBackend/controller/AccountController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,7 +54,7 @@ public class AccountController {
     return ResponseEntity.ok().build();
   }
   
-  @GetMapping("/login")
+  @PostMapping("/login")
   public ResponseEntity<Void> login(@RequestParam Map<String, String> params, HttpServletResponse response){
     
     String email = params.get("email");
